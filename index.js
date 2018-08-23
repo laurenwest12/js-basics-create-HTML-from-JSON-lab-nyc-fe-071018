c9cb95d50e751bc6bd9b352691e8ba16f562a479
Lauren West <laurenwest12@gmail.com>
2018-08-23 23:25:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", (e) => {
   // invoking a function here will make sure it happens on page load
   // your code here
-
+  const titanicButton = document.getElementById('Titanic')
+  const terminatorButton = document.getElementById('Terminator 2')
 });
