5752409338027788576fa370490ff137aa807c58
Lauren West <laurenwest12@gmail.com>
2018-08-24 00:16:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,9 +50,9 @@ function updateHTMLContent(movieName){
   castInnerHTML += "</ul>"
   cast.innerHTML = castInnerHTML
   
-  for (let review of currentMovies.reviews){
-    let userName = review.username
-    let content = review.content
+  for (let reviews of currentMovies.reviews){
+    let userName = reviews.username
+    let content = reviews.content
     reviewInnerHTML += "<p>" + username + ": " + content + "</p>" 
   }
   
